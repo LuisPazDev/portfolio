9026d5ccd135570b01003c68d146da604b2d26aa
Luis <luispazdev@gmail.com>
2023-04-09 17:59:31 -0500

    adding form inputs

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,23 +1,69 @@
+import { useState } from "react"
+import { db } from "../config/Firebase"
+import { addDoc, collection } from "firebase/firestore"
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 
 export const ContactForm = () => {
+    const [input, setInput] = useState({})
+
+    const handleInputChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setInput((values) => ({ ...values, [name]: value }))
+    }
+
+    const clearForm = () => {
+        document.getElementById("contact-form").reset()
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        console.log(input)
+        try {
+            const comment = collection(db, "contact")
+            const res = await addDoc(comment, input)
+            alert("Message sent successfully")
+            clearForm()
+            return res
+        } catch (error) {
+            alert("Error sending message")
+            clearForm()
+            console.log(error)
+        }
+    }
+
     return (
-        <Form className='container-contact-form'>
+        <Form
+            id='contact-form'
+            className='container-contact-form'
+            onSubmit={handleSubmit}>
             <h4 className='mb-3'>Send Message</h4>
             <Form.Group className='mb-3' controlId='formBasicName'>
                 <Form.Label>Name</Form.Label>
-                <Form.Control type='name' placeholder='Name' />
+                <Form.Control
+                    onChange={handleInputChange}
+                    type='text'
+                    name='name'
+                    placeholder='Name'
+                />
             </Form.Group>
             <Form.Group className='mb-3' controlId='formBasicEmail'>
                 <Form.Label>Email</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' />
+                <Form.Control
+                    onChange={handleInputChange}
+                    type='email'
+                    name='email'
+                    placeholder='Enter email'
+                />
             </Form.Group>
             <Form.Group className='mb-3' controlId='formBasicMessage'>
                 <Form.Label>Subject</Form.Label>
                 <Form.Control
+                    onChange={handleInputChange}
                     className='input-subject'
-                    type='message'
+                    type='text'
+                    name='message'
                     placeholder='Message'
                 />
             </Form.Group>
--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
+import { getFirestore } from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
+    apiKey: "AIzaSyBtECV5iPvZ6Yoo5WUBNPE3dF1eGKQcG0I",
+    authDomain: "luispazdev-bcaed.firebaseapp.com",
+    projectId: "luispazdev-bcaed",
+    storageBucket: "luispazdev-bcaed.appspot.com",
+    messagingSenderId: "726436576085",
+    appId: "1:726436576085:web:681f37f664097d27facddc",
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+export const db = getFirestore(app)
