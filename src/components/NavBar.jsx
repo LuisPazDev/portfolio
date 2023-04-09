fb20bd6d6930315b9890fd498f8e853620c6e92e
Luis <luispazdev@gmail.com>
2023-04-09 14:32:17 -0500

    modified:   src/components/NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,17 +7,24 @@ import logo from "../assets/luispazdevlogo.svg"
 export const NavBar = () => {
     return (
         <Container fluid className='nav-bar'>
-            <Navbar className='nav-bar' variant='dark'>
+            <Navbar
+                collapseOnSelect
+                expand='lg'
+                className='nav-bar'
+                variant='dark'>
                 <Container className='p-1'>
                     <Navbar.Brand href='#home'>
                         <img src={logo} alt='logo' className='logo' />
                     </Navbar.Brand>
-                    <Nav className='ms-auto'>
-                        <Nav.Link href='#about'>About</Nav.Link>
-                        <Nav.Link href='#skills'>Skills</Nav.Link>
-                        <Nav.Link href='#portfolio'>Portfolio</Nav.Link>
-                        <Nav.Link href='#contact'>Contact</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Collapse id='responsive-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <Nav.Link href='#about'>About</Nav.Link>
+                            <Nav.Link href='#skills'>Skills</Nav.Link>
+                            <Nav.Link href='#portfolio'>Portfolio</Nav.Link>
+                            <Nav.Link href='#contact'>Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </Container>
