4839d0e798ab3fb20318d28f96479fd341d2e2ee
Luis <luispazdev@gmail.com>
2023-06-27 10:44:51 -0500

    updating navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,23 +6,19 @@ import logo from "../assets/luispazdevlogo.svg";
 
 export const NavBar = () => {
   return (
-    <Container fluid className="nav-bar">
-      <Navbar collapseOnSelect expand="lg" className="nav-bar" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={logo} alt="logo" className="logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Container>
+    <Navbar collapseOnSelect expand="md" className="nav-bar" variant="dark">
+      <Container fluid className="ms-4 me-4">
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" className="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
